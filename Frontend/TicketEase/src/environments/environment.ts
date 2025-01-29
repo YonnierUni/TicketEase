296638390c2885a0acf88ebb3971b82d262f361a
YonnierUni <uniyonnier@gmail.com>
2025-01-29 02:08:53 -0500

    Update environment.ts

--- Frontend/TicketEase/src/environments/environment.ts
+++ Frontend/TicketEase/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiBaseUrl: process.env['API_BASE_URL'] || 'https://localhost',
-    email: process.env['EMAIL'] || "uniyonnier@gmail.com"
+    email: process.env['EMAIL'] || "uniyonnier@gmail.com",
   };
   
